b22c778e7e036c359e6822b029e1207556e20336
heeyeon9578 <heeyeon9578@naver.com>
2025-01-28 19:32:11 +0900

    bug: NEXT_PUBLIC_SITE_URL 이름 수정

--- src/socketIns.ts
+++ src/socketIns.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client";
-const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_SERVER_URL ||  "http://localhost:3000";
+const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SITE_URL ||  "http://localhost:3000";
 const socket: Socket = io(SOCKET_SERVER_URL, {
   path: "/api/socket",
 });
