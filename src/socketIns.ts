12389d659f22fce20afdbec105711ec27a17689e
heeyeon9578 <heeyeon9578@naver.com>
2025-01-28 19:44:03 +0900

    modified:   src/socketIns.ts

--- src/socketIns.ts
+++ src/socketIns.ts
@@ -1,5 +1,14 @@
 import { io, Socket } from "socket.io-client";
 const SOCKET_SERVER_URL = process.env.NEXT_PUBLIC_SITE_URL ||  "http://localhost:3000";
+console.log(` 
+  
+  
+  
+  SOCKET_SERVER_URL ${SOCKET_SERVER_URL}
+  
+  
+  
+  `)
 const socket: Socket = io(SOCKET_SERVER_URL, {
   path: "/api/socket",
 });
